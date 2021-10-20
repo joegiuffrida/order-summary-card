46e7948c1c060275ff09b57e91b339a763a8271b
Joe Giuffrida <Joey@Mac-mini.local>
2021-10-20 13:27:12 -0700

    fixing comments

--- components/OrderSummaryCard.js
+++ components/OrderSummaryCard.js
@@ -5,18 +5,18 @@ import Image from 'next/image';
 const OrderSummaryCard = () => {
   return (
     <div className={classes.body}>
-      // card component container
+      {/* card component container */}
       <div className={classes.container}>
-        // header pic w/headphones guy
+        {/* header pic w/headphones guy */}
         <header></header>
-        // main holds contents of the card
+        {/* main holds contents of the card */}
         <main>
           <h1>Order Summary</h1>
           <p>
             You can now listen to millions of songs, audiobooks, and podcasts on
             any device anywhere you like!
           </p>
-          // section is the gray box with music icon, annual plan, change link
+          {/* section is the gray box with music icon, annual plan, change link */}
           <section>
             <div className={classes.itunesLogo}>
               <Image src="/icon-music.svg" width="70%" height="25%" />
@@ -28,7 +28,7 @@ const OrderSummaryCard = () => {
               <Link href="/change">Change</Link>
             </div>
           </section>
-          // footer holds payment button and cancel order link
+          {/* footer holds payment button and cancel order link */}
           <footer className={classes.footer}>
             <button>Proceed to Payment</button>
             <Link href="/dummy">Cancel Order</Link>
